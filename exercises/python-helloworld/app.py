e25d27567ccdabe6e321abce0f2696b191596ee0
Miroslaw Kurzela <miroslaw.kurzela@gmail.com>
2021-06-25 15:43:20 +0200

    logger implemented
    logger implemented

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import json
+import logging
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+	## log line
+	app.logger.info('Main request successfull')
+	return "Hello World!"
 	
 @app.route("/status")
 def status():
@@ -16,6 +19,8 @@ def status():
 		status=200,
 		mimetype='application/json'
 	)
+	## log line
+	app.logger.info('Status request successfull')
 	return response
 
 @app.route("/metrics")
@@ -26,8 +31,23 @@ def metrics():
 		response = json.dumps(data),
 		status=200,
 		mimetype='application/json'
-	)	
+	)
+	## log line
+	app.logger.info('Metrics request successfull')	
 	return response
 	
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+
+	## logging to file
+	
+	logname = 'app.log'
+
+	logging.basicConfig(filename=logname,
+								filemode='a',
+								format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+								datefmt='%H:%M:%S',
+								level=logging.DEBUG)
+
+	logging.info("Running python app HELLO WORLD")
+
+	app.run(host='0.0.0.0')
